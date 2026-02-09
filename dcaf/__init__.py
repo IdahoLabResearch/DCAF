4ab2a6c8b309f70ebb1e3ba0f59b5cd1182330dd
dylan-mcdowell <Dylan.McDowell@inl.gov>
2026-02-09 11:45:08 -0700

    Add init file

---
+++ dcaf/__init__.py
@@ -0,0 +1,22 @@
+"""
+DCAF - Discounted Cash Flow Analysis Framework
+
+A Python package for financial modeling and cashflow analysis.
+"""
+
+from dcaf.core.cashflows import (
+    CashFlow,
+    CashFlowGroup,
+    CashFlowStream,
+    CashFlowTags,
+    Money,
+)
+
+
+__all__ = [
+    "CashFlow",
+    "CashFlowGroup",
+    "CashFlowStream",
+    "CashFlowTags",
+    "Money",
+]
